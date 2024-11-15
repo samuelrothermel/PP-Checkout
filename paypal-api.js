2ec1cda146fe79d1b94362e29475efd6c1abe884
Sam Rothermel <samuelrothermel@gmail.com>
2024-11-14 20:12:56 -0500

    first commit

---
+++ paypal-api.js
@@ -0,0 +1,129 @@
+import fetch from 'node-fetch';
+
+// set some important variables
+const { CLIENT_ID, APP_SECRET } = process.env;
+const base = 'https://api-m.sandbox.paypal.com';
+
+export const createOrder = async () => {
+  const purchaseAmount = '100.00'; // TODO: pull prices from a database
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders`;
+  const response = await fetch(url, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({
+      intent: 'CAPTURE',
+      purchase_units: [
+        {
+          amount: {
+            currency_code: 'USD',
+            value: purchaseAmount,
+          },
+        },
+      ],
+    }),
+  });
+
+  return handleResponse(response);
+};
+
+export const capturePayment = async orderId => {
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+  const response = await fetch(url, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return handleResponse(response);
+};
+
+export const createVaultSetupToken = async ({ paymentSource }) => {
+  const paymentSources = {
+    paypal: {
+      description: 'Description for PayPal to be shown to PayPal payer',
+      usage_pattern: 'IMMEDIATE',
+      usage_type: 'MERCHANT',
+      customer_type: 'CONSUMER',
+      experience_context: {
+        shipping_preference: 'NO_SHIPPING',
+        payment_method_preference: 'IMMEDIATE_PAYMENT_REQUIRED',
+        brand_name: 'EXAMPLE INC',
+        locale: 'en-US',
+        return_url: 'https://example.com/returnUrl',
+        cancel_url: 'https://example.com/cancelUrl',
+      },
+    },
+    card: {
+      experience_context: {
+        shipping_preference: 'NO_SHIPPING',
+      },
+    },
+  };
+
+  const response = await fetch(`${base}/v3/vault/setup-tokens`, {
+    method: 'post',
+    headers: {
+      'PayPal-Request-Id': Date.now().toString(),
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${await generateAccessToken()}`,
+    },
+    body: JSON.stringify({
+      payment_source: {
+        [paymentSource]: paymentSources[paymentSource],
+      },
+    }),
+  });
+
+  return handleResponse(response);
+};
+
+export const createVaultPaymentToken = async vaultSetupToken => {
+  const response = await fetch(`${base}/v3/vault/payment-tokens`, {
+    method: 'post',
+    headers: {
+      'PayPal-Request-Id': Date.now().toString(),
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${await generateAccessToken()}`,
+    },
+    body: JSON.stringify({
+      payment_source: {
+        token: {
+          id: vaultSetupToken,
+          type: 'SETUP_TOKEN',
+        },
+      },
+    }),
+  });
+
+  return handleResponse(response);
+};
+
+export const generateAccessToken = async () => {
+  const auth = Buffer.from(CLIENT_ID + ':' + APP_SECRET).toString('base64');
+  const response = await fetch(`${base}/v1/oauth2/token`, {
+    method: 'post',
+    body: 'grant_type=client_credentials',
+    headers: {
+      Authorization: `Basic ${auth}`,
+    },
+  });
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
+};
+
+const handleResponse = async response => {
+  if (response.status === 200 || response.status === 201) {
+    return response.json();
+  }
+
+  const error = new Error(await response.text());
+  error.status = response.status;
+  throw error;
+};
---
+++ server.js
@@ -0,0 +1,98 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import * as paypal from './paypal-api.js';
+
+const { PORT = 8888 } = process.env;
+
+const handleError = (res, error) => {
+  console.error(error);
+  res.status(error.status || 500).send(error.message);
+};
+
+const app = express();
+
+const corsOptions = {
+  credentials: true,
+  origin: ['http://localhost:8888'],
+};
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+
+app.get('/', async (req, res) => {
+  const clientId = process.env.CLIENT_ID;
+  try {
+    res.render('index', { clientId });
+  } catch (err) {
+    handleError(res, err);
+  }
+});
+
+app.get('/purchase', async (req, res) => {
+  const clientId = process.env.CLIENT_ID;
+  try {
+    res.render('purchase', { clientId });
+  } catch (err) {
+    handleError(res, err);
+  }
+});
+
+app.get('/vault', async (req, res) => {
+  const clientId = process.env.CLIENT_ID;
+  try {
+    res.render('vault', { clientId });
+  } catch (err) {
+    handleError(res, err);
+  }
+});
+
+// create order
+app.post('/api/orders', async (req, res) => {
+  try {
+    const order = await paypal.createOrder();
+    res.json(order);
+  } catch (err) {
+    handleError(res, err);
+  }
+});
+
+app.post('/api/vault/setup-token', async (req, res) => {
+  try {
+    const { paymentSource } = req.body;
+    const vaultSetupToken = await paypal.createVaultSetupToken({
+      paymentSource,
+    });
+    res.json(vaultSetupToken);
+  } catch (err) {
+    handleError(res, err);
+  }
+});
+
+app.post('/api/vault/payment-token/:vaultSetupToken', async (req, res) => {
+  try {
+    const { vaultSetupToken } = req.params;
+    const paymentToken = await paypal.createVaultPaymentToken(vaultSetupToken);
+    res.json(paymentToken);
+  } catch (err) {
+    handleError(res, err);
+  }
+});
+
+// capture payment
+app.post('/api/orders/:orderID/capture', async (req, res) => {
+  const { orderID } = req.params;
+  try {
+    const captureData = await paypal.capturePayment(orderID);
+    res.json(captureData);
+  } catch (err) {
+    handleError(res, err);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}/`);
+});
