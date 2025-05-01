99d5ee5f8cc068f9d12ce9d6d8a8237d70bb437c
Sam Rothermel <samuelrothermel@gmail.com>
2025-05-01 12:47:40 -0400

    Enable app switch preference for PayPal components in one-time payments cart

--- public/one-time-payments-cart-ql.js
+++ public/one-time-payments-cart-ql.js
@@ -164,6 +164,7 @@ function reloadPayPalComponents(newTotal) {
         style: {
           layout: 'vertical',
         },
+        appSwitchWhenAvailable: true,
         createOrder,
         onApprove,
         onCancel,
